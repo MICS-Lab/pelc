7519d54a1e08ae5b7916a7c7eb064675e71c473e
JasonMendoza2008 <lhotteromain@gmail.com>
2022-10-25 22:14:33 +0200

    Better isolated classes unit tests
    Signed-off-by: JasonMendoza2008 <lhotteromain@gmail.com>

--- tests/test_epitope_mismatches.py
+++ tests/test_epitope_mismatches.py
@@ -117,31 +117,42 @@ def test_epitope_comparison_isolated_classes() -> None:
         False,
     )
 
-    output_df: pd.DataFrame = pd.read_csv(f"{output_path}.csv", index_col="Index")
+    output_df_fn: pd.DataFrame = pd.read_csv(f"{output_path}.csv", index_col="Index")
 
-    for index_ in range(5, 2146):
-        # We can include index_ = 5 because we're talking about False Negatives here (all the eplet mismatches are
-        # compensated by B*35:01)
-        assert output_df.at[index_, "Epitopic Charge"] == 0
+    for index_ in range(1, 2146):
+        if index_ != 4:
+            # Index 4 is removed because this is an unknown (EpRegistry unknown) allele.
+            # All the others indices lead to an epitopic charge of 0 because there are NO class I mismatches except
+            # a B*14:10 that has been predicted as a B*14:02. However, this does not lead to any False Negatives because
+            # all eplets on B*14:10 are either present on B*14:02 or B*35:01 (the other HLA-B allele).
+            assert output_df_fn.at[index_, "Epitopic Charge"] == 0
 
     os.remove(f"{output_path}.csv")
 
-    # Only class II
+    # Only class II (False Positives)
+    donordf, recipientdf, output_path = base_loading("False Pos")
     compute_epitopic_charge(
         donordf,
         recipientdf,
         output_path,
-        OutputType.COUNT,
+        OutputType.DETAILS_AND_COUNT,
         False,
         True,
         False,
     )
 
-    output_df: pd.DataFrame = pd.read_csv(f"{output_path}.csv", index_col="Index")
+    output_df_fp: pd.DataFrame = pd.read_csv(f"{output_path}.csv", index_col="Index")
 
-    for index_ in range(5, 2146):
-        # We can include index_ = 5 because we're talking about False Negatives here (all the eplet mismatches are
+    for index_ in range(4, 2146):
+        # - We can include index_ = 4 because we didn't have an absurd allele (only on the False Negs sheet) this time
+        # - We can include index_ = 5 because we're talking about False Negatives here (all the eplet mismatches are
         # compensated by B*35:01)
-        assert output_df.at[index_, "Epitopic Charge"] == 0
+        assert output_df_fp.at[index_, "Epitopic Charge"] == 0
+
+    list_mismatches_5: list[str] = output_df_fp.at[5, "EpMismatches"].split(", ")
+    assert ("97W_ABC" not in list_mismatches_5)
+    # Present on B*14:02 (predicted) but not on B*14:10 (true typing) and isn't compensated by loci HLA-A or HLA-C.
+    # Therefore is a false positive.
+    # But here we only care about class II so it should not appear
 
     os.remove(f"{output_path}.csv")
