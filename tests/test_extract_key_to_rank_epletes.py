526265b72ee7776741a4fa05d88011353131f082
JasonMendoza2008 <lhotteromain@gmail.com>
2023-01-08 00:29:05 +0100

    Documented test_extract_key_to_rank_eplets
    Signed-off-by: JasonMendoza2008 <lhotteromain@gmail.com>

--- tests/test_extract_key_to_rank_epletes.py
+++ tests/test_extract_key_to_rank_epletes.py
@@ -2,7 +2,10 @@ import pecc.epitope_comparison_aux
 
 
 def test_extract_key_to_rank_eplets() -> None:
+    # interlocus2 eplets
     assert 1037 == pecc.epitope_comparison_aux._extract_key_to_rank_eplets("rqp37YA")
     assert 1140 == pecc.epitope_comparison_aux._extract_key_to_rank_eplets("rq140TV")
+
+    # ABC, DR, DQ or DP eplets
     assert 8 == pecc.epitope_comparison_aux._extract_key_to_rank_eplets("8L")
     assert 35 == pecc.epitope_comparison_aux._extract_key_to_rank_eplets("35FV")
