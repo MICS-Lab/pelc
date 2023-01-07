5efcc2a01bee4848f9a1ddd90b3a9bd8d471bc4b
JasonMendoza2008 <lhotteromain@gmail.com>
2023-01-08 00:28:39 +0100

    Better unit tests for _extract_key_to_rank_eplets
    Signed-off-by: JasonMendoza2008 <lhotteromain@gmail.com>

--- tests/test_extract_key_to_rank_epletes.py
+++ tests/test_extract_key_to_rank_epletes.py
@@ -4,3 +4,5 @@ import pecc.epitope_comparison_aux
 def test_extract_key_to_rank_eplets() -> None:
     assert 1037 == pecc.epitope_comparison_aux._extract_key_to_rank_eplets("rqp37YA")
     assert 1140 == pecc.epitope_comparison_aux._extract_key_to_rank_eplets("rq140TV")
+    assert 8 == pecc.epitope_comparison_aux._extract_key_to_rank_eplets("8L")
+    assert 35 == pecc.epitope_comparison_aux._extract_key_to_rank_eplets("35FV")
