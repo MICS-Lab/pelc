545dc1a819783f88bb449508e3086bc853ce9616
JasonMendoza2008 <lhotteromain@gmail.com>
2023-01-11 14:19:08 +0100

    Better unit_tests run files

--- unit_tests_mypy.py
+++ unit_tests_mypy.py
@@ -1,6 +1,7 @@
 import pytest
 
-retcode: int | pytest.ExitCode = pytest.main(["--mypy"])
+if __name__ == "__main__":
+    retcode: int | pytest.ExitCode = pytest.main(["--mypy"])
 
-if retcode == pytest.ExitCode.OK:
-    print("\n--- \nAll good.\n--- \n")
+    if retcode == pytest.ExitCode.OK:
+        print("\n--- \nAll good.\n--- \n")
--- unit_tests_simple.py
+++ unit_tests_simple.py
@@ -1,6 +1,7 @@
 import pytest
 
-retcode: int | pytest.ExitCode = pytest.main([])
+if __name__ == "__main__":
+    retcode: int | pytest.ExitCode = pytest.main([])
 
-if retcode == pytest.ExitCode.OK:
-    print("\n--- \nAll good.\n--- \n")
+    if retcode == pytest.ExitCode.OK:
+        print("\n--- \nAll good.\n--- \n")
