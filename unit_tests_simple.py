3e2e474b0feec45b033127d596f2abc5f9d840ac
JasonMendoza2008 <lhotteromain@gmail.com>
2023-01-17 10:31:53 +0100

    Added mypy support in unit_tests_simple.py

--- unit_tests_simple.py
+++ unit_tests_simple.py
@@ -1,7 +1,7 @@
 import pytest
 
 if __name__ == "__main__":
-    retcode: int | pytest.ExitCode = pytest.main([])
+    retcode: int | pytest.ExitCode = pytest.main(["--mypy"])
 
     if retcode == pytest.ExitCode.OK:
         print("\n--- \nAll good.\n--- \n")
