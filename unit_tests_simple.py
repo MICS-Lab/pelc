88ab4c7dedeb4275a6fcb13a89b1d511f1cb9127
JasonMendoza2008 <lhotteromain@gmail.com>
2023-01-23 14:45:43 +0100

    Fixed minor bug unit_tests_simple.py

--- unit_tests_simple.py
+++ unit_tests_simple.py
@@ -1,7 +1,7 @@
 import pytest
 
 if __name__ == "__main__":
-    retcode: int | pytest.ExitCode = pytest.main(["--mypy"])
+    retcode: int | pytest.ExitCode = pytest.main()
 
     if retcode == pytest.ExitCode.OK:
         print("\n--- \nAll good.\n--- \n")
