e169b0f0e52a38550faf57ce97d4ae252e54b3fa
JasonMendoza2008 <lhotteromain@gmail.com>
2023-01-23 18:14:10 +0100

    better unit test code

--- tests/test_simple_comparison.py
+++ tests/test_simple_comparison.py
@@ -4,6 +4,16 @@ import pytest
 
 from pelc.simple_comparison import simple_comparison
 
+
+def in_a6802_but_not_in_a6801(df: pd.DataFrame) -> None:
+    assert (len(df) == 2)
+
+    assert ("12M" in df.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
+    assert ("97R" in df.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
+    assert ("114H" in df.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
+    assert ("116Y" in df.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
+
+
 def test_simple_comparison_a68() -> None:
     # All eplets
     simple_comparison(
@@ -19,12 +29,7 @@ def test_simple_comparison_a68() -> None:
     )
     pytest_a68_simple_comparison_no_abv.set_index("Unnamed: 0", inplace=True)
 
-    assert (len(pytest_a68_simple_comparison_no_abv) == 2)
-
-    assert ("12M" in pytest_a68_simple_comparison_no_abv.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
-    assert ("97R" in pytest_a68_simple_comparison_no_abv.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
-    assert ("114H" in pytest_a68_simple_comparison_no_abv.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
-    assert ("116Y" in pytest_a68_simple_comparison_no_abv.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
+    in_a6802_but_not_in_a6801(pytest_a68_simple_comparison_no_abv)
 
     # Antibody verified only
     simple_comparison(
@@ -86,9 +91,4 @@ def test_returning_dataframe() -> None:
 
     assert (isinstance(simple_comparaison_return, pd.DataFrame))
 
-    assert (len(simple_comparaison_return) == 2)
-
-    assert ("12M" in simple_comparaison_return.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
-    assert ("97R" in simple_comparaison_return.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
-    assert ("114H" in simple_comparaison_return.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
-    assert ("116Y" in simple_comparaison_return.loc["In A*68:02 but not in A*68:01"]["EpMismatches"])
+    in_a6802_but_not_in_a6801(simple_comparaison_return)
