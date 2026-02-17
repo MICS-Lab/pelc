ad56e20cc84f6d449f7456dd4049040ffecb78b1
JasonMendoza2008 <lhotteromain@gmail.com>
2026-02-17 18:36:05 +0100

    Updated EpRegistry database and added unit tests.

--- tests/test_convert_to_eplets.py
+++ tests/test_convert_to_eplets.py
@@ -8,9 +8,9 @@ from pelc._open_epregistry_databases import (  # noqa
 )
 
 
-def test_convert_to_eplets() -> None:
+def test_convert_to_eplets_dqa1() -> None:
     """
-    Test the _convert_to_eplets function.
+    Test the _convert_to_eplets function on some DQA1 alleles.
     :return: Nothing
     """
     this_file_directory_path: str = os.path.dirname(os.path.realpath(__file__))
@@ -25,7 +25,6 @@ def test_convert_to_eplets() -> None:
         "2D_DQ",
         "25YT_DQ",
         "40GR_DQ",
-        "55R_DQ",
         "61FT_DQ",
         "66IL_DQ",
         "75S_DQ",
@@ -44,6 +43,45 @@ def test_convert_to_eplets() -> None:
     )
     assert sorted(output_list_dqa05_05) == sorted(expected_list)
 
+def test_convert_to_eplets_dqb1() -> None:
+    """
+    Test the _convert_to_eplets function on a DQB1 allele.
+    :return: Nothing
+    """
+    this_file_directory_path: str = os.path.dirname(os.path.realpath(__file__))
+    df_ref: pd.DataFrame = _open_epregistry_database(
+        f"{this_file_directory_path}{os.sep}..{os.sep}pelc{os.sep}data{os.sep}DQ.csv",
+        ["DQB1*", "DQA1*"],
+    )
+    df_data: pd.DataFrame = _open_ep_data(
+        f"{this_file_directory_path}{os.sep}..{os.sep}pelc"
+    )
+
+    output_list_dqb1_02_01: list[str] = _convert_to_eplets(
+        "DQB1*02:01", df_ref, "DQ", df_data, False, False
+    )
+    assert "2D_DQ" not in output_list_dqb1_02_01
+    assert "3S_DQ" in output_list_dqb1_02_01
+
+def test_convert_to_eplets_rare_c() -> None:
+    """
+    Test the _convert_to_eplets function on a rare C allele.
+    :return: Nothing
+    """
+    this_file_directory_path: str = os.path.dirname(os.path.realpath(__file__))
+    df_ref: pd.DataFrame = _open_epregistry_database(
+        f"{this_file_directory_path}{os.sep}..{os.sep}pelc{os.sep}data{os.sep}C.csv",
+        ["C*"],
+    )
+    df_data: pd.DataFrame = _open_ep_data(
+        f"{this_file_directory_path}{os.sep}..{os.sep}pelc"
+    )
+
+    output_list_c_18_24: list[str] = _convert_to_eplets(
+        "C*18:24", df_ref, "ABC", df_data, False, False
+    )
+    assert "1C_ABC" in output_list_c_18_24
+    assert "9F_ABC" not in output_list_c_18_24
 
 def test_convert_to_eplets_questionable() -> None:
     """
